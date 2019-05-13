0797d478dbf5438f81af59579a02c05468ce2ba1
hzz-pearl <943118906@qq.com>
2019-05-13 22:23:01 +0800

    哈哈哈H

---
+++ test/hw.js
@@ -0,0 +1,40 @@
+// for(var i =0;i<3;i++){
+
+// }
+// console.log(i);
+// for(let i =0;i<3;i++){
+
+// }
+// console.log(i);
+// const pen = 10;
+// pen = 20;
+// console.log(pen);
+// const num = {};
+// num.numble = 20;
+// console.log(num);
+// var obj = {name:"haha",age:22};
+// var {name,age}=obj;
+
+//函数参数的默认值
+// function hello(name = "zz"){
+//     console.log(name);
+    
+// }
+// // hello("sb");
+// hello();
+// 快捷添加属性
+// let name = "lee";
+// let age = 21;
+// let obj = {
+//     name,
+//     age
+// };
+// console.log(obj);
+//快捷给对象添加方法
+// let obj={
+//     sum(x,y){
+//         console.log(x+y);
+        
+//     }
+// }
+// obj.sum(10,20);
